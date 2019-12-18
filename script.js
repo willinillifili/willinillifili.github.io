ff5881f3505179134d16c464a0b99dd621f433e7
willy <aldarondo.42@gmail.com>
2019-12-18 13:09:32 -0400

    clasitronicos

--- script.js
+++ script.js
@@ -1,61 +1,27 @@
-function removeBanner() {
-  $(".bannerMain,.bannerSub, .button").css("display", "none");
-  $(".location").css("display", "none");
-  $(".contact").css("display", "none");  
-  $(".promoImage").css("display", "flex");
-  $("#promo").attr("src", "./assets/promoespecial.jpg");
-}
-
-function removeAll() {
-    $(".bannerMain,.bannerSub, .button").css("display", "none");
-    $(".promoImage").css("display", "none");
-    $(".location").css("display", "none");
-    $(".contact").css("display", "none");
-}
-
 $(document).ready(function(){
-  $(".button, .especiales").click(removeBanner);
+  $(".nav-item").hover(
+    function(){
+      $(this).children(".dropdown").css("display", "flex");
+    },
+    function(){
+      $(this).children(".dropdown").css("display", "none");
+    }
+  );
 
-  $(".localizacion").click(function() {
-    removeAll();
-    $(".location").css("display", "flex");
-  });
-
-  $(".contactenos").click(function(){
-    removeAll();
-    $(".contact").css("display", "flex");
+  $(".toggle").click(function() {
+    let menu = $("#nav-items");
+    let show = menu.attr("class");
+    if (show === "") menu.attr("class", "nav-items");
+    else menu.attr("class", "");
   })
 
-  $("#f1").click(function(){
-    removeBanner();
-    $("#promo").attr("src", "./assets/promoathmovil.jpg");
-  });
-
-  $("#f2").click(function(){
-    removeBanner();
-    $("#promo").attr("src", "./assets/promotexto.jpg");
-  });
-
-  $("#f3").click(function(){
-    removeBanner();
-    $("#promo").attr("src", "./assets/promodelivery.jpg");
-  });
-
-  $("#promo").click(function(){
-    src = $(this).attr('src');
-    $(".overlay").css("display", "flex");
-    $("#promoLarge").attr("src", src);
-  });
-
-  $(".back").click(function(){
-    $(".overlay").css("display", "none");
-  });
-
-  $(".feature").hover(function(){
-    var text = $(this).children("div");
-    text.css("color", "yellow");
-  }, function(){
-    var text = $(this).children("div");
-    text.css("color", "white");
+  $(".nav-item").click(function(){
+    var text = $(this).children("span").text().trim();
+    var pos = $(".cat-title:contains("+text+")").offset().top;
+    $("html, body").animate({
+      scrollTop: pos
+    });
+    $("#nav-items").attr("class", "");
   })
+  
 });
