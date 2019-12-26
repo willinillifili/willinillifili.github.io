8208907c2ba8f38275503737cd6273efb8793258
willy <aldarondo.42@gmail.com>
2019-12-26 15:39:03 -0400

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $(".nav-item").hover(
+  $(".nav-item").hover( //menu item hover
     function(){
       $(this).children(".dropdown").css("display", "flex");
     },
@@ -8,14 +8,20 @@ $(document).ready(function(){
     }
   );
 
-  $(".toggle").click(function() {
+  $(".toggle").click(function() { //mobile menu toggle
     let menu = $("#nav-items");
     let show = menu.attr("class");
-    if (show === "") menu.attr("class", "nav-items");
-    else menu.attr("class", "");
+    if (show === "") {
+      menu.css("display", "flex");
+      menu.attr("class", "nav-items");
+    }
+    else {
+      menu.css("display", "none");
+      menu.attr("class", "");
+    }
   })
 
-  $(".nav-item").click(function(){
+  $(".nav-item").click(function(){ //menu item navigate to corresponding tile
     var text = $(this).children("span").text().trim();
     var pos = $(".cat-title:contains("+text+")").offset().top;
     $("html, body").animate({
@@ -23,5 +29,32 @@ $(document).ready(function(){
     });
     $("#nav-items").attr("class", "");
   })
-  
+
+  $(".arrow").click(function(){ //main page categories scroll
+    var carousel;
+    var arrow = $(this);
+    if (arrow.attr("id") === "left") {
+      var carousel = $(this).prev().children("div");//select carousel
+      carousel.attr("class", "cat-center flex-row");//clean out previous effect
+      var animation = "scroll-left";
+    }
+    else {
+      var carousel = $(this).next().children("div");//select carousel
+      carousel.attr("class", "cat-center flex-row");//clean out previous effect
+      var animation = "scroll-right";
+    }
+    $(carousel).addClass(animation);//adds animation class
+  })
+
+  //yellow pages render directory
+  const categories = ["abogados", "aires acondicionados", "alquiler", "auto",
+    "auto essentials", "belleza y salud", "bienes raices", "camas", "cerrajero",
+  "contabilidad y consultoria", "contratista", "dealer", "ebanisteria",
+  "empleo", "empleos", "energia renovable", "estimador", "empleador"];
+
+  for (cat of categories) {
+    $("<div class='directory-item flex-column'><a href='./result.html'>"+cat+"</a></div>")
+    .insertBefore("#marker");
+  }
+
 });
